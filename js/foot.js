fac06f840a139e49c92271165e37ef2aaa76ddd6
xiaowaaao <3044506953@qq.com>
2024-01-22 16:38:28 +0800

    Site updated: 2024-01-22 16:38:28

--- js/foot.js
+++ js/foot.js
@@ -9,9 +9,9 @@ $(document).ready(function(e){
 
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
-$('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
+$('.framework-info').html('小破站已运行<span id="span_dt_dt" style="color: #fff;"></span>');
 window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("22/1/2024 24:0:0");
+BirthDay=new Date("22/1/2024 12:0:0");
 today=new Date();
 timeold=(today.getTime()-BirthDay.getTime());
 sectimeold=timeold/1000
